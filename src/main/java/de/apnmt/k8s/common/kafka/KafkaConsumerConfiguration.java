5e297a1b9d7d63f007d2c57c0e6cf9fe91020eed
tobi5775 <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>
2021-12-06 10:19:15 +0100

    add kafka support

---
+++ src/main/java/de/apnmt/k8s/common/kafka/KafkaConsumerConfiguration.java
@@ -0,0 +1,50 @@
+package de.apnmt.k8s.common.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+public class KafkaConsumerConfiguration {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("$application.kafka.group.id")
+    private String groupId;
+
+    private Map<String, Object> consumerConfigs() {
+        final Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, this.bootstrapServers);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+
+        // allows a pool of processes to divide the work of consuming and processing records
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, this.groupId);
+        return props;
+    }
+
+    @Bean
+    public ConsumerFactory<String, Object> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(this.consumerConfigs());
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(this.consumerFactory());
+        return factory;
+    }
+
+}
---
+++ src/main/java/de/apnmt/k8s/common/test/AbstractEventSenderIT.java
@@ -0,0 +1,64 @@
+package de.apnmt.k8s.common.test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.connect.json.JsonDeserializer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.KafkaMessageListenerContainer;
+import org.springframework.kafka.listener.MessageListener;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
+import org.springframework.kafka.test.utils.ContainerTestUtils;
+
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public abstract class AbstractEventSenderIT {
+
+    protected BlockingQueue<ConsumerRecord<String, Object>> records;
+
+    private KafkaMessageListenerContainer<String, String> container;
+
+    @Autowired
+    private EmbeddedKafkaBroker embeddedKafkaBroker;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void setUp() {
+        DefaultKafkaConsumerFactory<String, String> consumerFactory = new DefaultKafkaConsumerFactory<>(this.getConsumerProperties());
+        ContainerProperties containerProperties = new ContainerProperties(this.getTopic());
+        this.container = new KafkaMessageListenerContainer<>(consumerFactory, containerProperties);
+        this.records = new LinkedBlockingQueue<>();
+        this.container.setupMessageListener((MessageListener<String, Object>) this.records::add);
+        this.container.start();
+        ContainerTestUtils.waitForAssignment(this.container, this.embeddedKafkaBroker.getPartitionsPerTopic());
+    }
+
+    private Map<String, Object> getConsumerProperties() {
+        return Map.of(
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, this.embeddedKafkaBroker.getBrokersAsString(),
+            ConsumerConfig.GROUP_ID_CONFIG, "consumer",
+            ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true",
+            ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "10",
+            ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "60000",
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class,
+            ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        this.container.stop();
+    }
+
+    protected abstract String getTopic();
+
+}
