5cb600a0a63d04e261fa35ec6a1d1ac20668d014
tobi5775 <tobias.wittmann96@web.de>
2022-03-15 16:14:44 +0100

    trace communication via kafka

--- src/main/java/de/apnmt/k8s/common/kafka/KafkaConsumerConfiguration.java
+++ src/main/java/de/apnmt/k8s/common/kafka/KafkaConsumerConfiguration.java
@@ -1,5 +1,6 @@
 package de.apnmt.k8s.common.kafka;
 
+import brave.kafka.interceptor.TracingConsumerInterceptor;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -26,11 +27,31 @@ public class KafkaConsumerConfiguration {
     @Value("${spring.kafka.consumer.auto-startup}")
     private boolean autoStartup;
 
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Value("${spring.zipkin.base-url}")
+    private String zipkinBaseUrl;
+
+    @Value("${spring.zipkin.enabled}")
+    private boolean zipkinEnabled;
+
     private Map<String, Object> consumerConfigs() {
         final Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, this.bootstrapServers);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        if (zipkinEnabled) {
+            props.put(ConsumerConfig.INTERCEPTOR_CLASSES_CONFIG, TracingConsumerInterceptor.class);
+            props.put("zipkin.http.endpoint", zipkinBaseUrl + "/api/v2/spans");
+            props.put("zipkin.sender.type", "HTTP");
+            props.put("zipkin.encoding", "JSON");
+            props.put("zipkin.remote.service.name", "kafka");
+            props.put("zipkin.local.service.name", appName);
+            props.put("zipkin.trace.id.128bit.enabled", "true");
+            props.put("zipkin.sampler.rate", "1.0F");
+        }
 
         // allows a pool of processes to divide the work of consuming and processing records
         props.put(ConsumerConfig.GROUP_ID_CONFIG, this.groupId);
--- src/main/java/de/apnmt/k8s/common/kafka/KafkaEventSenderConfiguration.java
+++ src/main/java/de/apnmt/k8s/common/kafka/KafkaEventSenderConfiguration.java
@@ -1,5 +1,8 @@
 package de.apnmt.k8s.common.kafka;
 
+import brave.kafka.interceptor.TracingConsumerInterceptor;
+import brave.kafka.interceptor.TracingProducerInterceptor;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,11 +22,31 @@ public class KafkaEventSenderConfiguration {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Value("${spring.zipkin.base-url}")
+    private String zipkinBaseUrl;
+
+    @Value("${spring.zipkin.enabled}")
+    private boolean zipkinEnabled;
+
     private Map<String, Object> producerConfigs() {
         final Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, this.bootstrapServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        if (zipkinEnabled) {
+            props.put(ProducerConfig.INTERCEPTOR_CLASSES_CONFIG, TracingProducerInterceptor.class);
+            props.put("zipkin.http.endpoint", zipkinBaseUrl + "/api/v2/spans");
+            props.put("zipkin.sender.type", "HTTP");
+            props.put("zipkin.encoding", "JSON");
+            props.put("zipkin.remote.service.name", "kafka");
+            props.put("zipkin.local.service.name", appName);
+            props.put("zipkin.trace.id.128bit.enabled", "true");
+            props.put("zipkin.sampler.rate", "1.0F");
+        }
         return props;
     }
 
